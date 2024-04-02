580f56b3fd6197480327b5338a8148eb9e26c74a
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-02 20:06:13 +0700

    add export default

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export { default as hero1 } from './hero1.webp';
+export { default as hero2 } from './hero2.webp';
+export { default as hero3 } from './hero3.webp';
+export { default as hero4 } from './hero4.webp';
