95a76dd74259803a208de39ce33d8f4172aec4c4
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-03 20:22:08 +0700

    use customFetch from utils for a featured products

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,14 @@
 import { Hero } from '../components';
+import { customFetch } from '../utils';
+
+const url = '/products?featured=true';
+
+export const loader = async () => {
+	const resp = await customFetch(url);
+	const products = resp.data.data;
+	console.log(products);
+	return products;
+};
 
 const Landing = () => {
 	return (
