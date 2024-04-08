a2aeddf528924e274403dbb8129a0a1cd8c25d55
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-08 20:11:46 +0700

    complete the filters for products

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,5 +1,76 @@
+import { Form, useLoaderData, Link } from 'react-router-dom';
+import FormInput from './FormInput';
+import FormSelect from './FormSelect';
+import FormRange from './FormRange';
+import FormCheckbox from './FormCheckbox';
+
 const Filters = () => {
-	return <h1>Filters</h1>;
+	const { meta, params } = useLoaderData();
+	const { search, company, category, shipping, order, price } = params;
+
+	return (
+		<Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
+			{/* SEARCH */}
+			<FormInput
+				type='search'
+				label='Search product'
+				name='search'
+				size='input-sm'
+				defaultValue={search}
+			/>
+
+			{/* CATEGORIES */}
+			<FormSelect
+				label='Select category'
+				name='category'
+				list={meta.categories}
+				size='select-sm'
+				defaultValue={category}
+			/>
+
+			{/* COMPANIES */}
+			<FormSelect
+				label='Select company'
+				name='company'
+				list={meta.companies}
+				size='select-sm'
+				defaultValue={company}
+			/>
+
+			{/* ORDER */}
+			<FormSelect
+				label='sort by'
+				name='order'
+				list={['a-z', 'z-a', 'high', 'low']}
+				size='select-sm'
+				defaultValue={order}
+			/>
+
+			{/* RANGE PRICE */}
+			<FormRange
+				name='range'
+				label='select price'
+				size='range-sm'
+				price={price}
+			/>
+
+			{/* FREE SHIPPING */}
+			<FormCheckbox
+				name='shipping'
+				label='free shipping'
+				size='checkbox-sm'
+				defaultValue={shipping}
+			/>
+
+			{/* BUTTONS */}
+			<button type='submit' className='btn btn-primary btn-sm uppercase'>
+				search
+			</button>
+			<Link to='/products' className='btn btn-accent btn-sm uppercase'>
+				reset
+			</Link>
+		</Form>
+	);
 };
 
 export default Filters;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,11 +3,19 @@ import { customFetch } from '../utils';
 
 const url = '/products';
 
-export const loader = async ({ req }) => {
-	const resp = await customFetch(url);
+export const loader = async ({ request }) => {
+	// const params = new URL(request.url).searchParams;
+	// const search = params.get('search');
+
+	/* ONE LINE */
+	const params = Object.fromEntries([
+		...new URL(request.url).searchParams.entries(),
+	]);
+
+	const resp = await customFetch(url, { params });
 	const products = resp.data.data;
 	const meta = resp.data.meta;
-	return { products, meta };
+	return { products, meta, params };
 };
 
 const Products = () => {
