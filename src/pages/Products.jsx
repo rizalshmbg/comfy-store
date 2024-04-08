110c3f5fdb73d05042a7b7538d589c5402c4fe5e
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-08 11:18:45 +0700

    add loader for product component

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,23 @@
-const Products = () => {
-	return <h1>Products</h1>;
+import { Filters, PaginationContainer, ProductsContainer } from '../components';
+import { customFetch } from '../utils';
+
+const url = '/products';
+
+export const loader = async ({ req }) => {
+	const resp = await customFetch(url);
+	const products = resp.data.data;
+	const meta = resp.data.meta;
+	return { products, meta };
 };
+
+const Products = () => {
+	return (
+		<>
+			<Filters />
+			<ProductsContainer />
+			<PaginationContainer />
+		</>
+	);
+};
+
 export default Products;
