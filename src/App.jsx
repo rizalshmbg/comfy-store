98e567918a3419f67aacc1785b33fcd5b104518b
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-10 19:05:22 +0700

    add import store for parameter in login action

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import { loader as productsLoader } from './pages/Products';
 // actions
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
+import { store } from './store';
 
 const router = createBrowserRouter([
 	{
@@ -70,7 +71,7 @@ const router = createBrowserRouter([
 		path: '/login',
 		element: <Login />,
 		errorElement: <ErrorPage />,
-		action: loginAction,
+		action: loginAction(store),
 	},
 	{
 		path: '/register',
