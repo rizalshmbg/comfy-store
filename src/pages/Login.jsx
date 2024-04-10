a75abdf82f69813965ca4b45d7688c5bc82add03
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-10 19:15:54 +0700

    complete login for guest user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,6 +26,24 @@ export const action =
 	};
 
 const Login = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const loginAsGuestUser = async () => {
+		try {
+			const response = await customFetch.post('/auth/local', {
+				identifier: 'test@test.com',
+				password: 'secret',
+			});
+			dispatch(loginUser(response.data));
+			toast.success('Welcome guest user!');
+			navigate('/');
+		} catch (error) {
+			console.log(error);
+			toast.error('Guest user login error. Please try again!');
+		}
+	};
+
 	return (
 		<section className='h-screen grid place-items-center'>
 			<Form
@@ -33,22 +51,16 @@ const Login = () => {
 				className='card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4'
 			>
 				<h4 className='text-center text-3xl font-bold'>Login</h4>
-				<FormInput
-					type='email'
-					label='email'
-					name='identifier'
-					defaultValue='test@test.com'
-				/>
-				<FormInput
-					type='password'
-					label='password'
-					name='password'
-					defaultValue='secret'
-				/>
+				<FormInput type='email' label='email' name='identifier' />
+				<FormInput type='password' label='password' name='password' />
 				<div className='mt-4'>
 					<SubmitBtn text='login' />
 				</div>
-				<button type='button' className='btn btn-secondary btn-block uppercase'>
+				<button
+					type='button'
+					className='btn btn-secondary btn-block uppercase'
+					onClick={loginAsGuestUser}
+				>
 					guest user
 				</button>
 				<p className='text-center'>
