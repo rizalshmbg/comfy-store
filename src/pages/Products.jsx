d288b47e17bd872318b030554377e38e93f3c437
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-12 20:32:22 +0700

    complete all product query

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,6 +3,25 @@ import { customFetch } from '../utils';
 
 const url = '/products';
 
+const allProductsQuery = (queryParams) => {
+	const { search, category, company, sort, price, shipping, page } =
+		queryParams;
+
+	return {
+		queryKey: [
+			'products',
+			search ?? '',
+			category ?? 'all',
+			company ?? 'all',
+			sort ?? 'a-z',
+			price ?? 100000,
+			shipping ?? false,
+			page ?? 1,
+		],
+		queryFn: () => customFetch(url, { params: queryParams }),
+	};
+};
+
 export const loader =
 	(queryClient) =>
 	async ({ request }) => {
@@ -14,7 +33,7 @@ export const loader =
 			...new URL(request.url).searchParams.entries(),
 		]);
 
-		const resp = await customFetch(url, { params });
+		const resp = await queryClient.ensureQueryData(allProductsQuery(params));
 		const products = resp.data.data;
 		const meta = resp.data.meta;
 		return { products, meta, params };
