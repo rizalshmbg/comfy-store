f39439c29e6ac770a6b66361d4c608f65be90eed
rizalshmbg <sihombingrizal18@gmail.com>
2024-04-12 08:19:50 +0700

    create loader for orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,3 +1,39 @@
+import { redirect, useLoaderData } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { customFetch } from '../utils';
+import { OrdersList, PaginationContainer, SectionTitle } from '../components';
+
+export const loader =
+	(store) =>
+	async ({ request }) => {
+		const user = store.getState().userState.user;
+		if (!user) {
+			toast.warn('You must logged in to view orders');
+			return redirect('/login');
+		}
+
+		const params = Object.fromEntries([
+			...new URL(request.url).searchParams.entries(),
+		]);
+
+		try {
+			const response = await customFetch.get('/orders', {
+				params,
+				headers: {
+					Authorization: `Bearer ${user.token}`,
+				},
+			});
+			return { orders: response.data.data, meta: response.data.meta };
+		} catch (error) {
+			const errorMessage =
+				error?.response?.data?.error?.message ||
+				'There was an error placing your order';
+			toast.error(errorMessage);
+			if (error.response.status === 401 || 403) return redirect('/login');
+			return null;
+		}
+	};
+
 const Orders = () => {
 	return <h1>Orders</h1>;
 };
